65234a7a091516e9b7fe3049454a2c08dba9d192
ZosL98 <Davidkof72@gmail.com>
2025-03-02 21:05:14 +0100

    Add files via upload

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+       $connect = mysqli_connect('localhost', 'root', 'Podlaga55#', 'cms');
+
+       if(mysqli_connect_errno()) {
+        exit('failed to connect to MySQL');
+       }
+
+?>
+
+
+<?php
+       // $connect = mysqli_connect('mysql.db.mdbgo.com', 'zosl98_cmsdb', 'Podlaga55#', 'zosl98_cmsdb');
+
+       // if(mysqli_connect_errno()) {
+       //  exit('failed to connect to MySQL');
+       // }
+
+?>
